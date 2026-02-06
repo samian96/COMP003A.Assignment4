57d493ad5992faf504fed13539c45101a7fd380e
samian <sam.iannazzo@yahoo.com>
2026-02-06 00:59:02 -0800

    fixed comment

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -39,7 +39,9 @@ namespace COMP003A.Assignment4
                     if (step >= safety)
                     {
                         Console.WriteLine("\n Safety Limit Reached.");
-                        continue; // break ends the program, preventing an endless loop of message displaying
+                        continue; /* continue repeats this message if the step int reaches the safety number that is set
+                                   * stops the display of completed progress
+                                   */
                     }
 
                     step++;
