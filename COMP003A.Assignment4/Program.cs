504081a8eaecb525a6d11b392b219f2ec3a9dc59
samian <sam.iannazzo@yahoo.com>
2026-02-05 23:11:31 -0800

    deleted string and readline

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -9,12 +9,9 @@
             Console.WriteLine("2. View Progress");
 
             Console.WriteLine("3. Exit");
- 
-            string input = "";
 
             bool active = true;
 
-            int x = int.Parse(Console.ReadLine());
             // start of a while loop
             while (active)
             {
