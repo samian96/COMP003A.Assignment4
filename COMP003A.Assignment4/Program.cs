2cc240b7fd9fd718bf3839e821b92ebe9ce52162
samian <sam.iannazzo@yahoo.com>
2026-02-06 00:53:45 -0800

    adjusted string line 41

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -38,7 +38,7 @@ namespace COMP003A.Assignment4
                 {
                     if (step >= safety)
                     {
-                        Console.WriteLine("\n Safety Limit Reached");
+                        Console.WriteLine("\n Safety Limit Reached. Ending session.");
                         break; // break ends the program, preventing an endless loop of message displaying
                     }
 
